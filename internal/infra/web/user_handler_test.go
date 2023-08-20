3128d679c5221c9b6197608497635b3831eebf71
Luizfpereira <luizpereira.mec@outlook.com>
2023-08-20 12:09:18 -0300

    user handler test unfinished

--- internal/infra/web/user_handler_test.go
+++ internal/infra/web/user_handler_test.go
@@ -1,2 +1,22 @@
 package web
 
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type UserHandlerSuite struct {
+	suite.Suite
+}
+
+func TestUserHandlerSuite(t *testing.T) {
+	suite.Run(t, new(UserHandlerSuite))
+}
+
+//https://www.cloudbees.com/blog/testing-http-handlers-go - ler para testar
+
+func (u *UserHandlerSuite) TestGetUserByID() {
+	r, err := http.NewRequest("GET", "/users/1", nil)
+}
